a4adb06e251e6bde25d77b87d80fc2175b445b14
oska1204 <39046463+oska1204@users.noreply.github.com>
2020-05-27 03:03:03 +0200

    Made for passing html and data in attributes

---
+++ js/utils/html.js
@@ -0,0 +1,27 @@
+/*
+// REQUIREMENT: Pass HTML in attributes with attribute="${`html`}" as opposed to attribute="html"
+
+// Example:
+  const template = document.createElement('template')
+  template.innerHTML = html`
+    <div data-html="${`<a href="#">link</a>`}">
+    </div>
+  `
+  console.log(template)
+// Output:
+//  <template>
+//    #document-fragment
+//      <div data-html="<a href="#">link</a>">
+//        </div>
+//  </template>
+*/
+
+export default function html(strings, ...data) {
+	let val = ''
+	for (let i = 0; i < data.length; i++) {
+	  val += strings[i]
+		val += data[i].replace(/"/g, '&#0034')
+	}
+	val += strings[strings.length - 1]
+	return val
+}
