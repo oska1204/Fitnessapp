51090a7bdf351e348f216b4c1c53af1da0c003e4
Oskar <oska1204@edu.campusvejle.dk>
2020-06-25 20:10:58 +0200

    Scss clean up, and prefer color scheme fix

--- js/utils/toggle-theme.js
+++ js/utils/toggle-theme.js
@@ -1,13 +1,12 @@
-const prefersDark = matchMedia('(prefers-color-scheme: dark)').matches
-localStorage.setItem('darkTheme', prefersDark)
-
 export default function toggleTheme() {
     const classList = document.documentElement.classList
 
     classList.remove('dark-theme')
     classList.remove('light-theme')
 
-    const isDark =  JSON.parse(localStorage.getItem('darkTheme'))
+    let isDark =  JSON.parse(localStorage.getItem('darkTheme'))
+    if (isDark === null)
+        isDark = matchMedia('(prefers-color-scheme: dark)').matches
     switch (isDark) {
         case true:
             classList.add('light-theme')
