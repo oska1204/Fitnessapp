13fb94ac5e34adf1ffd47100393ca6897fbeea37
Oskar Paustian Andersen <oska1204@edu.campusvejle.dk>
2020-06-25 11:59:27 +0200

    Working on @media (prefers-color-scheme:dark)

--- js/utils/toggle-theme.js
+++ js/utils/toggle-theme.js
@@ -1,4 +1,5 @@
 export default function toggleTheme() {
+    // matchMedia('(prefers-color-scheme: dark)') jar jar is the key to everything
     const darkTheme = document.documentElement.classList.toggle('dark-theme')
     localStorage.setItem('darkTheme', darkTheme)
 }
