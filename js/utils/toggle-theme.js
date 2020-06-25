3fe235612f91d98fa2b31a7caa1abb3064b6ec65
Oskar <oska1204@edu.campusvejle.dk>
2020-06-25 19:46:52 +0200

    Added prefers color scheme support

--- js/utils/toggle-theme.js
+++ js/utils/toggle-theme.js
@@ -1,5 +1,21 @@
+const prefersDark = matchMedia('(prefers-color-scheme: dark)').matches
+localStorage.setItem('darkTheme', prefersDark)
+
 export default function toggleTheme() {
-    // matchMedia('(prefers-color-scheme: dark)') jar jar is the key to everything
-    const darkTheme = document.documentElement.classList.toggle('dark-theme')
-    localStorage.setItem('darkTheme', darkTheme)
+    const classList = document.documentElement.classList
+
+    classList.remove('dark-theme')
+    classList.remove('light-theme')
+
+    const isDark =  JSON.parse(localStorage.getItem('darkTheme'))
+    switch (isDark) {
+        case true:
+            classList.add('light-theme')
+            break;
+
+        case false:
+            classList.add('dark-theme')
+            break;
+    }
+    localStorage.setItem('darkTheme', !isDark)
 }
