8bf939b898c9936bebd52d24f37f9457d3c6040a
oska1204 <oska1204@edu.campusvejle.dk>
2020-11-17 17:10:20 +0100

    Generates import for web-components

---
+++ node-generate.js
@@ -0,0 +1,30 @@
+// Generates /js/utils/web-components.js that links to all the web-components in the /js/web-components folder
+
+// If you have node installed, open the console and run this:
+// node node-generate.js
+
+const fs = require('fs')
+
+function getFiles(dir, files_) {
+    files_ = files_ || [];
+    var files = fs.readdirSync(dir);
+    for (var i in files) {
+        var name = dir + '/' + files[i];
+        if (fs.statSync(name).isDirectory()) {
+            getFiles(name, files_);
+        } else {
+            if (name.match(/\.js$/))
+                files_.push(name);
+        }
+    }
+    return files_;
+}
+
+const file = getFiles('./js/web-components').map(e => `import '${e.slice(1)}'`).join('\n')
+
+fs.writeFile('./js/utils/web-components.js', file, err => {
+    if (err === null)
+        console.log('success')
+    else
+        console.error(err)
+})
