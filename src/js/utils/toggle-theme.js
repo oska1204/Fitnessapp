3299f0d469684864abeab6247dbfd36862412a70
oska1204 <oska1204@edu.campusvejle.dk>
2020-11-29 19:13:35 +0100

    "toggle-theme.js" now supports no cookies

--- src/js/utils/toggle-theme.js
+++ src/js/utils/toggle-theme.js
@@ -1,10 +1,15 @@
+let isDark = null
+try {
+    isDark =  JSON.parse(localStorage.getItem('darkTheme'))
+} catch (err) {
+    console.error(err)
+}
 export default function toggleTheme() {
     const classList = document.documentElement.classList
 
     classList.remove('dark-theme')
     classList.remove('light-theme')
 
-    let isDark =  JSON.parse(localStorage.getItem('darkTheme'))
     if (isDark === null)
         isDark = matchMedia('(prefers-color-scheme: dark)').matches
     switch (isDark) {
@@ -16,5 +21,6 @@ export default function toggleTheme() {
             classList.add('dark-theme')
             break;
     }
-    localStorage.setItem('darkTheme', !isDark)
+    isDark = !isDark
+    localStorage.setItem('darkTheme', isDark)
 }
